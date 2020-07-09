8c1e7a3b6623d5b9f414fd9291cd9dd0a2023cd3
writeair <1154515863@qq.com>
2020-07-09 12:00:47 +0800

    add time

--- fund.py
+++ fund.py
@@ -22,40 +22,42 @@ def getWorth(fscode):
     yes_price =jsonObj.get('dwjz')
     gs_price =jsonObj.get('gsz')
     gs_zf =jsonObj.get('gszzl')
-    return name,yes_price,gs_price,gs_zf
+    gs_time=jsonObj.get('gztime')
+    return name,yes_price,gs_price,gs_zf,gs_time
 allCode =["000404","003096","002851","001837","161725","000751","166002","161122"]                     # 基金的代码 
 share =  {'000404':'100.66','003096':'183.66','002851':'6.56','001837':'6.50','161725':'13.22','000751':'2.95','166002':'229.50','161122':'85.27'}   # 持有基金所对应的份额
 netWorthFile = open('./netWorth.csv','w')
 gs_Sumprofit=0
+gs_time=0
 for code in allCode:
   try:
-    name,yes_price,gs_price,gs_zf = getWorth(code)
+    name,yes_price,gs_price,gs_zf,gs_time = getWorth(code)
   except:
     continue
   gs_profit=round((float(gs_price)-float(yes_price))*float(share.get(code)), 2)
-  gs_Sumprofit=gs_profit+gs_Sumprofit
+  gs_Sumprofit=round(gs_profit+gs_Sumprofit, 2)
   print(code,name,"昨日净值："+yes_price,"估算值："+gs_price,"估算涨幅："+gs_zf+"%","估算收益："+str(gs_profit)+"元") 
   netWorthFile.write("\'"+code+"\',")  
   netWorthFile.write(name+","+yes_price+","+gs_price+","+gs_zf+","+str(gs_profit))
   netWorthFile.write("\n")
   #print("write "+code+"'s data success.")
 print("今日估算收益（150）："+str(gs_Sumprofit)+"元")
-allCode =["161725","217027","009549","000751","001548","162712"]                     # 基金的代码 
-share =  {'161725':'7.73','217027':'42.28','009549':'216','000751':'45.25','001548':'149.75','162712':'6.97'}   # 持有基金所对应的份额
+allCode =["161725","217027","009549","000751","001548","162712","110011","160221"]                     # 基金的代码 
+share =  {'161725':'7.73','217027':'42.28','009549':'216','000751':'45.25','001548':'149.75','162712':'6.97','110011':'7.62','160221':'0'}   # 持有基金所对应的份额
 
 gs_Sumprofit1=0
 for code in allCode:
   try:
-    name,yes_price,gs_price,gs_zf = getWorth(code)
+    name,yes_price,gs_price,gs_zf,gs_time = getWorth(code)
   except:
     continue
   gs_profit=round((float(gs_price)-float(yes_price))*float(share.get(code)), 2)
-  gs_Sumprofit1=gs_profit+gs_Sumprofit1
+  gs_Sumprofit1=round(gs_profit+gs_Sumprofit1, 2)
   print(code,name,"昨日净值："+yes_price,"估算值："+gs_price,"估算涨幅："+gs_zf+"%","估算收益："+str(gs_profit)+"元") 
   netWorthFile.write("\'"+code+"\',")  
   netWorthFile.write(name+","+yes_price+","+gs_price+","+gs_zf+","+str(gs_profit))
   netWorthFile.write("\n")
  # print("write "+code+"'s data success.")
 print("今日估算收益（159）："+str(gs_Sumprofit1)+"元")
-print("今日估算总收益："+str(gs_Sumprofit+gs_Sumprofit1)+"元")
+print("今日估算总收益："+str(gs_Sumprofit+gs_Sumprofit1)+"元"+",估算时间："+gs_time)
 netWorthFile.close()
